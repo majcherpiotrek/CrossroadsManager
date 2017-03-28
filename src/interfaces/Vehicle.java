62db65578ba9d87c4bfc32a091ab315de56e1c77
piotrmajcher94 <piotr.majcher94@gmail.com>
2017-03-28 20:44:49 +0200

    Moving the objects example (Controller.java)

--- src/interfaces/Vehicle.java
+++ src/interfaces/Vehicle.java
@@ -8,7 +8,6 @@ import javafx.geometry.Point2D;
  */
 public interface Vehicle {
 
-    void move(double targetX, double targetY);
     Point2D getPosition();
-    void setPosition(Point2D position);
+    void setPosition(Point2D pos);
 }
--- src/simulationview/Controller.java
+++ src/simulationview/Controller.java
@@ -1,30 +1,54 @@
 package simulationview;
 
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+import sun.awt.windows.ThemeReader;
+
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
 import java.util.ResourceBundle;
 
 
 public class Controller implements Initializable{
 
-    private @FXML Canvas canvas;
-    private GraphicsContext gc;
+    private @FXML
+    StackPane stackPane;
+
+    private @FXML
+    Canvas canvas;
+
+    private @FXML
+    AnchorPane anchorPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        gc = canvas.getGraphicsContext2D();
-        gc.setFill(Color.AQUA);
-        gc.setStroke(Color.RED);
 
-        gc.strokeLine(5,5,50,50);
-        gc.strokeLine(10,0,60,50);
-        gc.fillOval(50,50,20,20);
-        gc.fillOval(60,40,20,20);
-        gc.fillOval(3,0, 20,20);
+        for(int i=0; i < 10; i++)
+            anchorPane.getChildren().add(new Rectangle(20+20*i,100,10,10));
+
+        ArrayList<TranslateTransition> transList = new ArrayList<>();
+        for(Node node : anchorPane.getChildren()){
+            transList.add(new TranslateTransition());
+            transList.get(transList.size()-1).setDuration(Duration.seconds(10));
+            transList.get(transList.size()-1).setNode(node);
+            transList.get(transList.size()-1).setToY(200);
+            transList.get(transList.size()-1).play();
+        }
+
+
+
+
     }
 }
