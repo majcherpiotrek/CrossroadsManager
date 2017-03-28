3cb5d9a56de7a525437120e1830ae14f10c87bde
Olaf Krawczyk <radeko94@gmail.com>
2017-03-28 10:07:55 +0200

    crossroad class added

---
+++ src/simulationelements/Crossroad.java
@@ -0,0 +1,15 @@
+package simulationelements;
+
+
+import interfaces.Drawable;
+import javafx.scene.canvas.GraphicsContext;
+
+
+
+public class Crossroad implements Drawable
+{
+    @Override
+    public void draw(GraphicsContext gc) {
+
+    }
+}
