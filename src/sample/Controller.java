219473eeb46c80599c7a51a711c983e76688f283
piotrek <piotr.majcher94@gmail.com>
2017-03-28 08:33:33 +0200

    Added sample canvas

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable{
+
+    private @FXML Canvas canvas;
+    private GraphicsContext gc;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.AQUA);
+        gc.setStroke(Color.RED);
+
+        gc.strokeLine(0,0,50,50);
+        gc.fillOval(50,50,20,20);
+    }
 }
